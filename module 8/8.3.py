ca9d31172c0e84aba2d90c3d394db3372b0e1baf
Idalskie <123402237+Idalskie@users.noreply.github.com>
2023-03-23 15:35:27 -0400

    Create 8.3.py

---
+++ module 8/8.3.py
@@ -0,0 +1,8 @@
+file = input("Enter a file:")
+fhand = open(file , "f")
+count = 0 
+for line in fhand:
+    words = line.split()
+    #print('Debug:", words")
+    if len(words) == 0 or len(words)<3 or words[0] != 'From' : continue
+    print(words[2])
