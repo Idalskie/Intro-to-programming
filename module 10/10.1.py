e9bd40a75a9ca45ecff4b28c9358003a2f8c1c64
Idalskie <123402237+Idalskie@users.noreply.github.com>
2023-04-20 15:21:05 -0400

    Create 10.1.py

---
+++ module 10/10.1.py
@@ -0,0 +1,20 @@
+n = dict()
+w = list()
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+for line in handle: 
+    words = line.split()
+    if len(words)<2 or words[0] != 'From':
+        continue 
+    else:
+        if hour not in n:
+            n[words[1]] = 1
+        else:
+            n[words[1]] +=1
+for key, val in list(n.items()):
+    w.append((key, val))
+w.sort(reverse = True)
+for key, val in w[:1]:
+    print(key,val)
